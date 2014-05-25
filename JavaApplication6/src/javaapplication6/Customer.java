11c91014a2701e0180515e6b0ed06f12f68a900c
SLeopold <SebastianLeopold@fun-byte.com>
2014-05-25 08:57:13 +0200

    Replace with Temp Query von getTotalCharge Replace With Temp Query von getTotalFrequentRenterPoints

--- JavaApplication6/src/javaapplication6/Customer.java
+++ JavaApplication6/src/javaapplication6/Customer.java
@@ -17,8 +17,6 @@ class Customer {
         return name;
     };
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
         Enumeration enum_rentals = rentals.elements();	    
         String result = "Rental Record for " + this.getName() + "\n";
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
@@ -31,8 +29,30 @@ class Customer {
             totalAmount += each.getCharge();
         }
         //add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+        result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
+        return result;
+    }
+    
+    private double getTotalCharge()
+    {
+        double result = 0;
+        Enumeration enum_rentals = rentals.elements();
+        while (enum_rentals.hasMoreElements()) {
+            Rental each = (Rental) enum_rentals.nextElement();
+            result += each.getCharge();
+        }
+        return result;
+    }
+    
+    private int getTotalFrequentRenterPoints()
+    {
+        int result = 0;
+        Enumeration enum_rentals = rentals.elements();
+        while (enum_rentals.hasMoreElements()) {
+            Rental each = (Rental) enum_rentals.nextElement();
+            result += each.getFrequentRenterPoints();
+        }
         return result;
     }
 }
